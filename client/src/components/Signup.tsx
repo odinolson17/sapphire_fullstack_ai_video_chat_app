623c798f805d6e468c4921a0cf0b855138be76f8
odinolson17 <odinolsonsbooks@gmail.com>
2024-03-19 00:21:15 -0400

    Updated css on the sign up form

--- client/src/components/Signup.tsx
+++ client/src/components/Signup.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import './styles/signup.css';
 
 function Signup () {
   const navigate = useNavigate();
@@ -24,7 +25,7 @@ function Signup () {
           })
         });
         const response = await request.json();
-        if (response) navigate('/home');
+        if (response) navigate('/home', { state: name });
         else navigate('/failedsignup');
       } catch {
         navigate('/failedsignup');
@@ -39,42 +40,74 @@ function Signup () {
 
   return (
     <>
-      <form onSubmit={createUser}>
-        <input 
-          placeholder="Your name"
-          value={name}
-          autoComplete='on'
-          onChange={(e) => setName(e.target.value)}
-          required={true}
-        />
-        <input 
-          placeholder="Email"
-          type="email"
-          value={email}
-          autoComplete='on'
-          onChange={(e) => setEmail(e.target.value)}
-          required={true}
-        />
-        <input 
-          placeholder="Password"
-          type="password"
-          value={password}
-          autoComplete='on'
-          onChange={(e) => setPassword(e.target.value)}
-          required={true}
-        />
-        <input 
-          placeholder="Enter your password again"
-          type="password"
-          value={secondPassword}
-          autoComplete='on'
-          onChange={(e) =>  setSecondPassword(e.target.value)}
-          required={true}
-        />
-        <button>
-        Create
-        </button>
-      </form>
+      <div className='to-the-left'>
+            <h1 className='logo'>SAPPHIRE</h1>
+      </div>
+      <div className='center-content'>
+        <div className='sign-up'>
+          <div>
+            <h2>Create Account</h2>
+          </div>
+          <div>
+            <form onSubmit={createUser}>
+              <div className='underline'>
+                <label>Display Name:</label>
+                <input 
+                  className="signininput"
+                  value={name}
+                  autoComplete='on'
+                  onChange={(e) => setName(e.target.value)}
+                  required={true}
+                />
+              </div>
+              <br />
+              <div className='underline'>
+                <label>Email:</label>
+                <input 
+                  className="signininput"
+                  type="email"
+                  value={email}
+                  autoComplete='on'
+                  onChange={(e) => setEmail(e.target.value)}
+                  required={true}
+                />
+              </div>
+              <br />
+              <div>
+                <div className='underline'>
+                  <label>Password:</label>
+                  <input 
+                    className="signininput"
+                    type="password"
+                    value={password}
+                    autoComplete='on'
+                    onChange={(e) => setPassword(e.target.value)}
+                    required={true}
+                  />
+                </div>
+                <br />
+                <label className='littletext'>Must be eight characters or more</label>
+              </div>
+              <br />
+              <div className='underline'>
+                <label>Confirm Password:</label>
+                <input
+                  className="signininput" 
+                  type="password"
+                  value={secondPassword}
+                  autoComplete='on'
+                  onChange={(e) => setSecondPassword(e.target.value)}
+                  required={true}
+                />
+              </div>
+              <br/>
+              <div className='to-the-right'>
+                <button className="signinbutton">Create</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
