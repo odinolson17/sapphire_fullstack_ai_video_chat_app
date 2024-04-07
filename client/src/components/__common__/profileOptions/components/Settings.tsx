8e4da1491caa4e0cc2ad02be224046cacda32130
odinolson17 <odinolsonsbooks@gmail.com>
2024-04-07 13:22:32 -0400

    Fixed status

--- client/src/components/__common__/profileOptions/components/Settings.tsx
+++ client/src/components/__common__/profileOptions/components/Settings.tsx
@@ -1,17 +1,23 @@
 import ProfilePicture from "./profilepicture/ProfilePicture";
 
 import { settingsStore } from "../../../../store/settings/settingsStore";
+import { statusStore } from "../../../../store/status/statusStore";
 import { useNavigate } from "react-router-dom";
-import { useRecoilValue } from "recoil";
+import { useRecoilState, useRecoilValue } from "recoil";
 
 function Settings () {
   const navigate = useNavigate();
   const state = useRecoilValue(settingsStore);
+  // refreshes the contacts on homepage
+  const [, setStatus] = useRecoilState(statusStore);
 
   return (
     <>
       <h1>settings</h1>
-      <button onClick={() => navigate('/home')}>
+      <button onClick={() => {
+        navigate('/home');
+        setStatus(true);
+      }}>
         Home
       </button>
       <br /><br />
